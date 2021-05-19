e8660db76bd5d5f887ce264bc4ae42f40b59274d
mostafasayed-dev <mostafasayed.fathi@gmail.com>
2021-05-19 11:07:02 +0200

    Adding initial dashboard component

--- shopy-admin/src/app/app.module.ts
+++ shopy-admin/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './_layout/header/header.component';
 import { SideNavComponent } from './_layout/side-nav/side-nav.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SideNavComponent
+    SideNavComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
